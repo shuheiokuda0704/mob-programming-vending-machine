488b1a7d0ba73c74bfbd7b99d1db94a793a61581
sivanpayyadakath <sivan.payyadakath@monstar-lab.com>
2020-07-17 18:26:15 +0900

    insert money method

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,3 +1,7 @@
 class VendingMachine
 
+  def insert_money(amount)
+    [10, 50, 100, 500, 1000].include?(amount)
+  end
+
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -9,8 +9,17 @@ RSpec.describe 'Vending Machine' do
     expect(RUBY_VERSION).to eq "2.6.3"
   end
 
+  let(:vending_machine){ VendingMachine.new }
+
   example "vending machine should be initialised" do
-    vending_machine = VendingMachine.new
     expect(vending_machine.class).to eq VendingMachine
+  end
+
+  example "can insert 10, 50, 100, 500 coins and 1000 yen bills" do
+    expect(vending_machine.insert_money(10)).to be true
+  end
+
+  example "cannot insert other than 10, 50, 100, 500 coins and 1000 yen bills" do
+    expect(vending_machine.insert_money(1)).to be false
   end
 end
